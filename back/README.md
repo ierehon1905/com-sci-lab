195f86bf933cea14a5e19004a67d4e27c73aa03e
ierehon1905 <ierehon1905@gmail.com>
2020-02-16 22:39:12 +0300

    доступ по сети

--- back/README.md
+++ back/README.md
@@ -8,7 +8,7 @@
     ```
 4. Запускаешь проект
     ```bash
-    env FLASK_APP=main.py flask run
+    python3 main.py
     ```
 
 Пиши если не получается
--- back/main.py
+++ back/main.py
@@ -7,3 +7,5 @@ app = Flask(__name__)
 def hello():
     name = request.args.get("name", "World")
     return f'Hello, {escape(name)}!'
+
+app.run(host='0.0.0.0')
