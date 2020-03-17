df50f58ac37fce2f1adad933fd43f4dfd5730374
ierehon1905 <ierehon1905@gmail.com>
2020-03-17 20:56:39 +0300

    пояснение бека

--- back/app.py
+++ back/app.py
@@ -1,41 +1,67 @@
-import os
-from flask import Flask, render_template, request
-from flask_cors import CORS
-import crypto
+import os  # импортируем библиотеку для работы с ОС
+from flask import Flask, render_template, request  # импортируем библиотеку для работы с веб приложеним
+from flask_cors import \
+    CORS  # импортируем библиотеку для работы cors'ами, чтобы не заморачиваться с безопасностью запросов
+import crypto  # импортируем наш модуль для работы с блокчейном
 
-path = os.path.join('.', '..', 'front', 'build')
-abs_path = os.path.abspath(path)
-abs_static = os.path.join(abs_path, 'static')
+path = os.path.join('.', '..', 'front', 'build')  # относительный путь собранного фронтенда
+abs_path = os.path.abspath(path)  # делаем абсолютный путь, для избежания проблем с совместимостью ОС
+abs_static = os.path.join(abs_path, 'static')  # путь до статики: стили и скомпилированного js
 
-app = Flask(__name__, static_folder=abs_static, template_folder=abs_path)
-CORS(app)
+app = Flask(__name__, static_folder=abs_static, template_folder=abs_path)  # инициализация веб приложения
+CORS(app)  # инициализация модуля cors
 
 
-@app.route('/')
+# Далее определяется маршрутизация
+
+@app.route('/')  # декоратор, указывам корневой маршрут
 def index():
+    """
+    Возвращаем файл index.html,
+    хоть и не используем функционал шаблонизатора,
+    просто так легче, достаточно правильно инициализировать приложение
+    """
     return render_template('index.html')
 
 
 @app.route('/create-acc', methods=['GET', 'POST'])
 def create_acc():
+    """
+    Для обоих методов запроса.
+    Если метод POST пытаемя из запроса JSON взять поле **secret**
+    """
+    secret = ''
     if request.method == 'POST':
-        secret = 'test secret'
+        try:
+            secret = request.json['secret']
+        except Exception as e:
+            print(e)
+            return 'Invalid secret', 400  # Если не удалось извлеч параметр, отвучаем со статус-кодом 400 (Bad Request)
     return crypto.create_acc(secret)
 
 
 @app.route('/get-balance/')
 @app.route('/get-balance/<address>')
 def get_balance(address=None):
+    """
+    Можно запрашивать как с, так и без параметра,
+    но при отсутствии кидаем 400,
+    в другом случае обращаемся к crypto
+    """
     if address is None:
-        return 'Пустой адрес'
+        return 'Пустой адрес', 400
     return str(crypto.get_balance(address))
 
 
 @app.route('/get-block')
 @app.route('/get-block/<block>')
 def get_block(block):
+    """
+    Можно запрашикать без номера блока.
+    """
     return crypto.get_block(block)
 
 
+# Можно запускать как и с помощью flask CLI, так и с помощью обычного питона. Это для питона.
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=8081)
--- back/crypto.py
+++ back/crypto.py
@@ -1,45 +1,46 @@
 from eth_account import Account
-import os
 from web3 import Web3, exceptions
-from typing import Union
 
+# Инициализация API
+# Для работы API нужна нода Ethereum
+# Мы используем сервис предоставляющий ноды: https://infura.io
+#
 w3 = Web3(Web3.HTTPProvider('https://ropsten.infura.io/v3/48f2e1b1628c4a7786576bb5505de814'))
 
 
-# # Секреты не пушим
-# print(os.environ['WEB3_INFURA_PROJECT_ID'])
-
-# # acct = Account.create('test acc')
-# # print(acct.address)
-# # print(acct.key)
-
-# # print(w3.eth.blockNumber)
-# # print(w3.eth.getBlock('latest'))
-
-# print(w3.isAddress('0x0f551e8e47b311F4b82Bad8e90e5Ef5D6f457528'))
-# print(w3.eth.getBalance('0x0f551e8e47b311F4b82Bad8e90e5Ef5D6f457528', 'latest'))
-# print(w3.eth.defaultAccount)
-# print(w3.eth.gasPrice)
-
 def get_balance(address: str):
+    """
+    Получение баланса на адресе
+    По умолчанию на момент последнего блока
+    """
     try:
         print('Getting balance for ', address)
-        return w3.eth.getBalance(address, 'latest')
+        return w3.eth.getBalance(address, 'latest')  # Само обращение к API
     except exceptions.InvalidAddress:
         return 'Неверный адрес'
 
 
 def create_acc(secret: str):
-    acc = Account.create(secret)
+    """
+    Создаем новый случайный аккаунт.
+    Возвращаем его адрес и ключ
+    """
+    acc = Account.create(secret)  # Само обращение к API
     return {"key": w3.toHex(acc.key), "address": acc.address}
 
 
 def get_block(block_number: str):
+    """
+    Получаем информацию о конкретном блоке.
+    Если не задан номер блока (или значение не числовое), то последний на данный момент 'latest'.
+    Выбираем параметры даты, номера, сложности.
+    Возвращаем
+    """
     if block_number.isnumeric():
         block_number = int(block_number)
     else:
         block_number = 'latest'
-    block = w3.eth.getBlock(block_number)
+    block = w3.eth.getBlock(block_number)  # Само обращение к API
     d = dict(
         timestamp=block['timestamp'],
         number=block['number'],
